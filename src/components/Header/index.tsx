09f4ec65cf2897ef2d3f3cf8165620dcf4e73790
doryski <dominatorskyyy@gmail.com>
2020-11-06 17:39:06 +0100

    made minor changes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import React, { lazy, useRef } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import MenuIcon from '@material-ui/icons/Menu'
 import { IconButton } from '@material-ui/core'
@@ -11,75 +11,93 @@ import { Link } from 'react-router-dom'
 import theme, { textColors } from '../../theme'
 import useDialogHandler from '../../helpers/useDialogHandler'
 import DrawerComponent from './DrawerComponent'
+import useDeviceDetect from '../../helpers/useDeviceDetect'
 
 const Header = () => {
-	const {
-		isDialogOpen: isSideBarOpen,
-		close: closeSideBar,
-		toggle: toggleSideBar,
-	} = useDialogHandler(false)
+    const {
+        isDialogOpen: isSideBarOpen,
+        close: closeSideBar,
+        toggle: toggleSideBar,
+    } = useDialogHandler(false)
+    const isMobile = useDeviceDetect(1105)
 
-	return (
-		<Container>
-			<Link to='/'>
-				<LogoWrapper>
-					<JustjoinLogo />
-				</LogoWrapper>
-			</Link>
-			<Typography
-				color={textColors.span}
-				fWeight={theme.fontWeight[400]}
-			>
-				#1 Job Board for IT industry in Poland
-			</Typography>
-			<Navigation />
-			<Wrapper>
-				<AddOffer />
-				<CustomButton
-					fWeight={theme.fontWeight[400]}
-					icon
-					pink
-					padding='0.375em 0.625em 0.375em 1.125em'
-					margin='0 0.9375em 0 0'
-				>
-					Sign in
-				</CustomButton>
-				<IconButton onClick={toggleSideBar}>
-					<StyledMenuIcon />
-				</IconButton>
-			</Wrapper>
-			{isSideBarOpen && (
-				<DrawerComponent
-					handleClose={closeSideBar}
-					isOpen={isSideBarOpen}
-				/>
-			)}
-		</Container>
-	)
+    return (
+        <Container>
+            <Link to='/'>
+                <LogoWrapper>
+                    <JustjoinLogo />
+                </LogoWrapper>
+            </Link>
+            {!isMobile && (
+                <>
+                    <Typography
+                        color={textColors.span}
+                        fWeight={theme.fontWeight[400]}
+                    >
+                        #1 Job Board for IT industry in Poland
+                    </Typography>
+                    <Navigation />
+                </>
+            )}
+            <Wrapper>
+                <AddOffer />
+                <CustomButton
+                    fWeight={theme.fontWeight[400]}
+                    icon
+                    pink
+                    padding='0.375em 0.625em 0.375em 1.125em'
+                    margin='0 0.9375em 0 0'
+                >
+                    Sign in
+                </CustomButton>
+
+                <Wrapper>
+                    <IconButton onClick={toggleSideBar}>
+                        <StyledMenuIcon />
+                    </IconButton>
+                </Wrapper>
+            </Wrapper>
+
+            {isSideBarOpen && (
+                <DrawerComponent
+                    handleClose={closeSideBar}
+                    isOpen={isSideBarOpen}
+                />
+            )}
+        </Container>
+    )
 }
 export const Container = styled.header`
-	min-height: 68px;
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	background: ${({ theme }) => theme.colors.primary};
-	border-bottom: 1px solid ${({ theme }) => theme.colors.divider};
-	width: 100%;
+    min-height: 68px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background: ${({ theme }) => theme.colors.primary};
+    border-bottom: 1px solid ${({ theme }) => theme.colors.divider};
+    width: 100%;
 `
 
 export const LogoWrapper = styled.div`
-	width: 120px;
-	float: left;
-	margin: 7px 15px 0px 25px;
+    width: 120px;
+    float: left;
+    margin: 7px 15px 0px 25px;
 `
 export const Wrapper = styled.div`
-	display: flex;
-	height: 38px;
-	margin-right: 0.75em;
-	align-items: center;
+    display: flex;
+    height: 38px;
+    margin-right: 0.75em;
+    align-items: center;
 `
 const StyledMenuIcon = styled(MenuIcon)`
-	color: ${({ theme }) => theme.colors.span};
+    color: ${({ theme }) => theme.colors.span};
+    @media (max-width: 850px) {
+        width: 1.5em !important;
+        height: 1.5em !important;
+    }
+    @media (max-width: 600px) {
+        width: 1.75em !important;
+        height: 1.75em !important;
+    }
 `
 
 export default Header
